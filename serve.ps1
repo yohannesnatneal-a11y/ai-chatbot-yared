f49078984bb88b6e95e1adb5faf0d2ea23515210
yohannesnatneal-a11y <yohannesnatneal@gmail.com>
2026-09-18 05:54:18 -0700

    commi

---
+++ serve.ps1
@@ -0,0 +1,41 @@
+$port = 8080
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:$port/")
+$listener.Start()
+Write-Host "Server running on http://localhost:$port/ ..."
+
+$mimeTypes = @{
+    ".html" = "text/html; charset=utf-8"
+    ".css"  = "text/css; charset=utf-8"
+    ".js"   = "application/javascript; charset=utf-8"
+    ".json" = "application/json; charset=utf-8"
+    ".png"  = "image/png"
+    ".svg"  = "image/svg+xml"
+}
+
+while ($listener.IsListening) {
+    $context = $listener.GetContext()
+    $request = $context.Request
+    $response = $context.Response
+
+    $path = $request.Url.LocalPath
+    if ($path -eq "/") { $path = "/index.html" }
+    
+    $filePath = Join-Path $PSScriptRoot ($path.TrimStart("/").Replace("/", "\"))
+    
+    if (Test-Path $filePath -PathType Leaf) {
+        $ext = [System.IO.Path]::GetExtension($filePath).ToLower()
+        $contentType = if ($mimeTypes.ContainsKey($ext)) { $mimeTypes[$ext] } else { "application/octet-stream" }
+        $response.ContentType = $contentType
+        $response.Headers.Add("Access-Control-Allow-Origin", "*")
+        
+        $bytes = [System.IO.File]::ReadAllBytes($filePath)
+        $response.ContentLength64 = $bytes.Length
+        $response.OutputStream.Write($bytes, 0, $bytes.Length)
+    } else {
+        $response.StatusCode = 404
+        $msg = [System.Text.Encoding]::UTF8.GetBytes("404 Not Found")
+        $response.OutputStream.Write($msg, 0, $msg.Length)
+    }
+    $response.OutputStream.Close()
+}
